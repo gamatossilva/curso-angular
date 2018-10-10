2572fc773ae21c7ded7c9aebb2dc4301ef940173
Guilherme Augusto <guilherme_amsilva@hotmail.com>
2018-10-10 18:53:19 -0300

    first commit

---
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnInit {
+
+  @Input() valorInicial: number = 10;
+
+  constructor() {
+    this.log('construtor');
+  }
+
+  ngOnChanges() {
+    this.log('ngOnChanges');
+  }
+
+  ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  ngOnCheck() {
+    this.log('ngOnCheck');
+  }
+
+  ngAfterContentInit() {
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    this.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string) {
+    console.log(hook);
+  }
+
+}
